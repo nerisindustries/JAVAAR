b998a3ff935128c265f5ca78b14d080627545580
DFN <daviferreiraneris@gmail.com>
2026-03-16 11:40:38 -0300

    exercicio 1 foi completado com sucesso

---
+++ Ta.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ta {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Olá, informe um número");
+        int numero = leitor.nextInt();
+
+        String resultado = (numero % 2 == 0) ? "par" : "impar";
+        System.out.println("este numero é "+ numero);
+
+    }
+
+
+}
