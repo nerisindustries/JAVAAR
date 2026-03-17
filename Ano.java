4c95b7bd3cd82f0d26562ee3f42b6a10a4e78c5a
DFN <daviferreiraneris@gmail.com>
2026-03-17 10:34:31 -0300

    deu certo, foi bem legal de fazer

--- Ano.java
+++ Ano.java
@@ -1,8 +1,57 @@
 import java.util.Scanner;
 
-public class Ano {
+
+
+public class Ano{
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número de um a 10:");
+        int n1 = leitor.nextInt();
+
+
+
+switch (n1){
+
+case 1:
+System.out.println("O Resultado é :I");
+break;
+case 2:
+    System.out.println("O Resultado  : II");
+break;
+case 3:
+System.out.println("O Resultado é: III");
+break;
+case 4:
+System.out.println("O Resultado é: IV ");
+break;
+case 5:
+System.out.println("O Resultado é:V ");
+break;
+case 6:
+System.out.println("O Resultado é: VI");
+break;
+case 7:
+System.out.println("O Resultado é: VII");
+break;
+case 8:
+System.out.println("O Resultado é: VIII");
+break;
+case 9:
+System.out.println("O Resultado é:IX ");
+break;
+case 10:
+System.out.println("O Resultado é: X ");
+break;
+default:
+       System.out.println("Iforme Números de um a 10 apenas");
+
+
+}
+
+   }
+    
+}
         
         
         
@@ -10,7 +59,4 @@ public class Ano {
         
         
         
-        }   
         
-        
-        }
