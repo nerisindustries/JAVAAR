619dc31c6bbf3576902a03fac256b840cf46f5b7
DFN <daviferreiraneris@gmail.com>
2026-03-16 22:40:24 -0300

    Após corrigir alguns erros deu certo

--- Ca.java
+++ Ca.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-public class Tt {
+public class Ca {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("Opa, digite um número:");
         double num = leitor.nextDouble();
-Int resc =(num>0)?num+"esste número é positivo":(num=0)""
+String resc =(num>0)?num+" Este número é positivo":(num==0)?num+" Este número é nulo":num+" Este numero é negativo";
+System.out.println(resc);
 
     }
 }
